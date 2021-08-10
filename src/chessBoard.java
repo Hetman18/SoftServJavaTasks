690818b230149e6330e10ad6074c6a910db858ec
Hetman <gggomelll@gmail.com>
2021-08-10 17:12:21 +0300

    New reposytory for SoftServ Tasks, and first Task with corrections

--- src/chessBoard.java
+++ src/chessBoard.java
@@ -7,43 +7,42 @@ public class chessBoard {
     protected String chessBoardHeight = sc.nextLine();
     protected String chessBoardWidth = sc.nextLine();
 
-    public String inputValidationHeight() {
+    public boolean inputValidationHeight() {
         boolean inputHeight = chessBoardHeight.matches("\\d{2}");
         while (inputHeight = false) {
             System.out.println("Incorrect input value!");
             continue;
         }
-        return chessBoardHeight;
+        return inputHeight;
     }
 
-    public String inputValidationWidth() {
+    public boolean inputValidationWidth() {
         boolean inputWidth = chessBoardWidth.matches("\\d{2}");
         while (inputWidth = false){
             System.out.println("Incorrect input value, try againe!");
             continue;
         }
-        return chessBoardWidth;
+        return inputWidth;
     }
-        public String createChessBoard () {
+    public String createChessBoard () {
         inputValidationHeight();
         inputValidationWidth();
-            int height = Integer.parseInt(chessBoardHeight);
-            for (int i = 0; i < height; i++) {
-                if (height % 2 == 0) {
-                    System.out.println(" ");
-                } else {
-                    System.out.println("*");
-                }
+        int height = Integer.parseInt(chessBoardHeight);
+        for (int i = 0; i < height; i++) {
+            if (i % 2 == 0) {
+                System.out.println(" ");
+            } else {
+                System.out.println("*");
+            }
             int width = Integer.parseInt(chessBoardWidth);
             for (int j = 0; j < width; j++) {
-                if (width % 2 == 0) {
+                if (j % 2 == 0) {
                     System.out.println("*");
                 } else {
                     System.out.println(" ");
                 }
             }
         }
-            return createChessBoard();
+        return createChessBoard();
     }
 }
-
