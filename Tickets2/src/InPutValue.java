8bc636088230a8d2f6ea6b78c385b544320f4178
Hetman <gggomelll@gmail.com>
2021-08-29 20:58:40 +0300

    End chess board, sequence, triangle sorter, envelope analyse, file parser, happy tickets 2222

--- Tickets2/src/InPutValue.java
+++ Tickets2/src/InPutValue.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 public class InPutValue {
     Scanner sc = new Scanner(System.in);
 
-    public int getMinMax() {
+    public int getMin() {
+        System.out.println("Enter minimal value of ticket: ");
+        return sc.nextInt();
+    }
+
+    public int getMax() {
+        System.out.println("Enter maximal value of ticket: ");
         return sc.nextInt();
     }
 }
--- Tickets2/src/TicketsMain.java
+++ Tickets2/src/TicketsMain.java
@@ -2,7 +2,7 @@ public class TicketsMain {
 
     public static void main(String[] args) {
         InPutValue inPut = new InPutValue();
-        Tickets ticket = new Tickets(inPut.getMinMax(), inPut.getMinMax());
+        Tickets ticket = new Tickets(inPut.getMin(), inPut.getMax());
         ticket.outHappyEasyTickets();
     }
 }
